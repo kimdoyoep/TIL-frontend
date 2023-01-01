67dae66816f9fcd24344359011e612b4e2d5d5e3
Kimdoyeop <dpdp39000@gmail.com>
2023-01-01 22:59:41 +0900

    Study javascript

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ const pobi = [a, b];
 const crong = [c, d];
 
 
-function pobiScoreAdd(n) {
+function ScoreAdd(n) {
   var sum = 0;
-  //n = n.toString();
+  n = n.toString();
+  const nod = n.length;
 
-  for(i = 0; i < n.length; i){
+  for(i = 0; i < nod; i++){
     sum += n%10;
     n = Math.floor(n/10);
   }
@@ -19,9 +20,37 @@ function pobiScoreAdd(n) {
   return sum;
 }
 
+function ScoreMul(n) {
+  var sum = 1;
+  n = n.toString();
+  const nod = n.length;
+
+  for(i = 0; i < nod; i++){
+    sum *= n%10;
+    n = Math.floor(n/10);
+  }
+
+  return sum;
+}
+
+const A1 = ScoreAdd(a);
+const A2 = ScoreMul(a);
+
+const B1 = ScoreAdd(b);
+const B2 = ScoreMul(b);
+
+const C1 = ScoreAdd(c);
+const c2 = ScoreMul(c);
+
+const D1 = ScoreAdd(d);
+const D2 = ScoreMul(d);
+
+
+
 console.log(a);
-console.log(pobiScoreAdd(a));
-console.log(a.length);
+console.log(ScoreAdd(a));
+console.log(ScoreMul(a));
+console.log(A1);
 
 
 
