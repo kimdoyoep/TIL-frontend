2f7852aca2f6f542a9c7cf3694c2c2b3d3cac28a
Kimdoyeop <dpdp39000@gmail.com>
2023-01-01 22:38:01 +0900

    Study javascript

--- main.js
+++ main.js
@@ -6,9 +6,22 @@ const d = Math.floor(Math.random()*401);
 const pobi = [a, b];
 const crong = [c, d];
 
-console.log(pobi);
-console.log(crong);
 
+function pobiScoreAdd(n) {
+  var sum = 0;
+  //n = n.toString();
+
+  for(i = 0; i < n.length; i){
+    sum += n%10;
+    n = Math.floor(n/10);
+  }
+
+  return sum;
+}
+
+console.log(a);
+console.log(pobiScoreAdd(a));
+console.log(a.length);
 
 
 
