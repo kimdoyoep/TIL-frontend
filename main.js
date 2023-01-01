1752cb91618815f6f367a394264734beabb1dceb
Kimdoyeop <dpdp39000@gmail.com>
2023-01-01 21:16:48 +0900

    Study javascript :: Math.random

---
+++ main.js
@@ -0,0 +1,14 @@
+const a = Math.floor(Math.random()*401);
+const b = Math.floor(Math.random()*401);
+const c = Math.floor(Math.random()*401);
+const d = Math.floor(Math.random()*401);
+
+const pobi = [a, b];
+const crong = [c, d];
+
+console.log(pobi);
+console.log(crong);
+
+
+
+
