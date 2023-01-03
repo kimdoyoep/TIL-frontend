5c93f2397e78965cf5499d38c122ab4c594f4be6
Kimdoyeop <dpdp39000@gmail.com>
2023-01-03 16:17:46 +0900

    Study javascript

--- main04.js
+++ main04.js
@@ -1,9 +1,30 @@
-//
-//
+const word = "i love you";
+
+// ReverseWord = word => {
+//   const normalWord = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//   const copyNormalWord = [...normalWord];
+//   const reverseWord = copyNormalWord.reverse();
+//   const fnWord = [...word];
+
+//   for(i = 0; i < normalWord.length; i++){
+//     if(normalWord[i] !== reverseWord[i]){
+//       normalWord[i] = reverseWord[i];
+//     }
+//   }
+// }
 
 const normalWord = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const copyNormalWord = [...normalWord];
-const reverseWord = copyNormalWord.reverse();
+  const copyNormalWord = [...normalWord];
+  const reverseWord = copyNormalWord.reverse();
+  const fnWord = [...word];
 
-console.log(normalWord);
-console.log(reverseWord);
+  for(i = 0; i < normalWord.length; i++){
+    if(normalWord[i] !== reverseWord[i]){
+      normalWord[i] = reverseWord[i];
+    }
+  }
+
+console.log(normalWord)
+
+
+
