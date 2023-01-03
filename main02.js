6b292b37f5b25e57382e16191155c6efd1d049b4
Kimdoyeop <dpdp39000@gmail.com>
2023-01-03 15:50:33 +0900

    Study javascript

--- main02.js
+++ main02.js
@@ -1,22 +1,22 @@
-const cryptogram = "heloolohh";
-const cryptogramArr = [...cryptogram];
+// const cryptogram = "heloolohh";
+// const cryptogramArr = [...cryptogram];
 
-if(cryptogramArr.length >= 1000){
-  alert("error : 최대 1000자까지 입력할 수 있습니다.");
-}
+// if(cryptogramArr.length >= 1000){
+//   alert("error : 최대 1000자까지 입력할 수 있습니다.");
+// }
 
-console.log(cryptogramArr);
+// console.log(cryptogramArr);
 
-Del = (String) => {
-  for(k = 0; k < String.length; k++){
-    for(i = 0; i < String.length; i++){
-      if(String[i] === String[i+1]){
-        String.splice(i,2);
-      }
-    }
-  }
+// Del = (String) => {
+//   for(k = 0; k < String.length; k++){
+//     for(i = 0; i < String.length; i++){
+//       if(String[i] === String[i+1]){
+//         String.splice(i,2);
+//       }
+//     }
+//   }
   
-  return String;
-}
+//   return String;
+// }
 
-console.log(Del(cryptogramArr));
+// console.log(Del(cryptogramArr));
---
+++ main04.js
@@ -0,0 +1,9 @@
+//
+//
+
+const normalWord = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const copyNormalWord = [...normalWord];
+const reverseWord = copyNormalWord.reverse();
+
+console.log(normalWord);
+console.log(reverseWord);
