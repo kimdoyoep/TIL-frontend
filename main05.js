4ab09f07701789cc66af49fd3a8a4feec6637e6b
Kimdoyeop <dpdp39000@gmail.com>
2023-01-04 20:02:44 +0900

    Study javascript

--- main05.js
+++ main05.js
@@ -1,22 +1,24 @@
-// const money = Math.floor(Math.random()*1000000);
+const money = Math.floor(Math.random()*1000001);
 
-// CalcMoney = money => {
-//   const moneyToStirng = money.toString();
-//   const sum = 0;
-//   const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  
-//   for(i = 0; i < moneyToStirng.length; i++){
-//     sum = money%10;
-//     arr.splice(moneyToStirng.lenth - i, 1, sum);
-//     money = Math.florr(money/10);
-//   }
-//   return arr.reverse();
-// }
+CalcMoney = money => {
+  const moneyToStirng = money.toString();
+  let sum = null;
+  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const arr2 = [];
+
+  for(i = 0; i < moneyToStirng.length; i++){
+    sum = money%10;
+    arr2.unshift(sum);
+    money = Math.floor(money/10);
+  }
+
+  for(k = 0; k < arr2.length; k++){
+    arr.splice(arr2.length-k, 1, arr2[k]);
+  }
+  return arr.filter((x) => x !== 0).concat(arr.filter((x) => x === 0)).reverse();
+}
 
 
-// console.log(`money : ${money}`);
-// console.log(`result : ${CalcMoney(money)}`);
+console.log(`money : ${money}`);
+console.log(`result : ${CalcMoney(money)}`);
 
-const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-arr.splice(1,0,1,1)
-console.log(arr);
