33ec96b779d537d04f4764c18b15316dd89b2374
Kimdoyeop <dpdp39000@gmail.com>
2023-01-04 20:20:09 +0900

    Study javascript

--- main05.js
+++ main05.js
@@ -1,24 +1,26 @@
 const money = Math.floor(Math.random()*1000001);
 
 CalcMoney = money => {
-  const moneyToStirng = money.toString();
-  let sum = null;
-  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  const arr2 = [];
+  
+  
+    for(i = 0; i < money; i++){
+      if(money - 50000 > 0){
+        money = money - 50000;
+      }else{
+        break;
+      }
+    }
 
-  for(i = 0; i < moneyToStirng.length; i++){
-    sum = money%10;
-    arr2.unshift(sum);
-    money = Math.floor(money/10);
-  }
+    for(k = 0; k < money; k++){
+      if(money - 10000 > 0){
+        money = money - 10000;
+      }else{
+        break;
+      }
+    }
 
-  for(k = 0; k < arr2.length; k++){
-    arr.splice(arr2.length-k, 1, arr2[k]);
-  }
-  return arr.filter((x) => x !== 0).concat(arr.filter((x) => x === 0)).reverse();
+    return k;
 }
 
-
-console.log(`money : ${money}`);
-console.log(`result : ${CalcMoney(money)}`);
-
+console.log(CalcMoney(money))
+console.log(money);
