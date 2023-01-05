c29cec4c665748be5a6adb019539d0097cc0cca2
Kimdoyeop <dpdp39000@gmail.com>
2023-01-05 20:05:15 +0900

    Study javascript

--- main06.js
+++ main06.js
@@ -1,4 +1,7 @@
-const storage = [];
+const storage = ['dp@email.com, 도도', 'jjss@email.com, 심스', 'pp@email.com, 피피', 'suu@email.com, 수우우'];
+const arr = [];
+const arr2 = [];
+
 
 class Student{
 
@@ -8,7 +11,6 @@ class Student{
   }
 
   makeMail = () => {
-    const arr = [];
     const mail = this.email;
     const name = this.name;
     const mailToString = mail.toString();
@@ -29,17 +31,11 @@ class Student{
       console.log('한글로만 입력합니다.')
     }else{
       arr.push(`${mail}@email.com, ${name}`);
-      console.log(arr);
-      if(arr.length > 0)
-      storage.push(arr.toString());
-      console.log(storage);
+      console.log(arr)
     }
   }
   
 }
-
-const aa = [];
-console.log(aa.length)
 
 const student01 = new Student('dpdp','도엽지');
 const student02 = new Student('jsjs', '자스');
@@ -50,4 +46,25 @@ student02.makeMail();
 student03.makeMail();
 student04.makeMail();
 
+console.log(storage);
 
+duplicateTest = (storage, arr) => {
+  for(k = 0; k < arr.length; k++){
+    for(i = 0; i < storage.length; i++){
+      const storageNum = storage[i];
+      const storageArr = [...storageNum];
+      const arrNum = arr[k];
+      const arrArr = [...arrNum];
+      for(j = 0; j < 14; j++){
+        if(storageArr[j] === arrArr[j] && storageArr[j+1] === arrArr[j+1]){
+          arr2.storage[i];
+          console.log('성공했노 ㅋ');
+        }else{
+          continue;
+        }
+      }
+    }
+  }
+}
+
+duplicateTest(storage, arr);
