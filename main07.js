615fd0ab990673517a1921888387d90e58378f36
Kimdoyeop <dpdp39000@gmail.com>
2023-01-07 22:55:48 +0900

    study javascript

--- main07.js
+++ main07.js
@@ -1,3 +1,5 @@
+const userArr = [];
+
 class User {
   constructor(user) {
     this.user = user;
@@ -6,17 +8,19 @@ class User {
   userLength = () => {
     const user = this.user;
     if (user.length < 0 || user.length > 30) {
-      console.log("user의 길이는 1이상 30이하로 지정합니다.");
-      break;
+      return console.log("user의 길이는 1이상 30이하로 지정합니다.");
     }
-    return (console.log(`${user}`))
+    this.userName();
+  };
+
+  userName = () => {
+    const user = this.user;
+    userArr.push(user);
   };
 }
 
-const user = new User(
-  "mrko"
-);
-user.userLength();
+const user1 = new User("mrko");
+user1.userLength();
 
 const friends = [
   ["donut", "andole"],
@@ -26,3 +30,32 @@ const friends = [
   ["shakevan", "jun"],
   ["shakevan", "mrko"],
 ];
+
+addFriendAndScore = (user, friends) => {
+  const sum = 0;
+
+  for (let i = 0; i < friends.length; i++) {
+    const x = friends[i];
+    const y = user;
+    chooseFriend(x, y);
+  }
+};
+
+chooseFriend = (x, y) => {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === y) {
+      console.log(`3`);
+    }
+  }
+};
+
+addFriendAndScore(userArr[0], friends);
+
+function tt() {
+  if (userArr[0] === friends[1][1]) {
+    return console.log(`맞는데 왜그래`);
+  }
+  return console.log(`gg`);
+}
+
+tt();
