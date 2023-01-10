a155ce31ca392a010f5dc1f18394d41660efb7aa
Kimdoyeop <dpdp39000@gmail.com>
2023-01-10 17:16:04 +0900

    Study javascript

--- mian08.js
+++ mian08.js
@@ -14,18 +14,14 @@ shuffle = (arr) => {
 
 shuffle(zeroToNine);
 
-console.log(zeroToNine);
-
-const computerArr = [];
+const computerArr = []; //컴퓨터(상대방) 숫자 정렬.
 
 makeArrComputer = (arr) => {
   for (let i = 0; i < 3; i++) {
     arr.push(zeroToNine[i]);
     if (arr[0] === 0) {
-      console.log(`ss`);
       arr[0] = zeroToNine[i + 1];
       zeroToNine.splice(i + 1, 1);
-      console.log(zeroToNine);
     }
   }
 };
@@ -33,22 +29,20 @@ makeArrComputer = (arr) => {
 makeArrComputer(computerArr);
 console.log(computerArr);
 
-// makeArrComputer(computerArr);
+const input = prompt(
+  "게임을 시작합니다. (서로 다른 수로 이루어진 3자리 수를 입력하세요.)"
+);
+const inputArr = [...input];
 
-// console.log(computerArr);
+textCondition = () => {
+  const text = "서로 다른 3자리 자연수로 입력합니다.";
+  if (input === /[^가-힣]/ || input === /[^a-zA-Z]/ || inputArr.length > 3) {
+    alert(text);
+  }
+};
 
-// const readline = require("readline");
+textCondition();
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// rl.on("line", (line) => {
-//   console.log("input: ", line);
-//   rl.close();
-// });
-
-// rl.on("close", () => {
-//   process.exit();
-// });
+// strike = input => {
+//   for(i = 0; i < computerArr.length;)
+// }
